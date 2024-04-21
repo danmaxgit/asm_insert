d2a8a7553c45d214b32a7da10e5cc13c1d770752
danmaxgit <danmax11@yandex.ru>
2024-04-21 21:53:58 +0300

    code refactor

--- src/main.c
+++ src/main.c
@@ -6,25 +6,24 @@ int main() {
    uint64_t n = 4, sum = 0, index = 0;
     scanf("%ld", &n);
     printf("%ld\n", n);
-    asm ("\
-        movq $0, %3; \
-        movq %1, %%rcx; \
-    mainloop: \
-        movq %2, %%rax; \
-        incq %%rax; \
-        mulq %2; \
-        movq $2, %%rbx; \
-        divq %%rbx, %%rax; \
-        incq %%rax; \
-        movq %3, %%rbx; \
-        addq %%rax, %%rbx; \
-        movq %%rbx, %3; \
-        movq %2, %%rax; \
-        incq %%rax; \
-        movq %%rax, %2; \
-    loopq mainloop; \
-        movq %3, %%rax; \
-        movq %%rax, %0;"
+    asm (
+        "movq %1, %%rcx;"
+    "mainloop:"
+        "movq %2, %%rax;"
+        "incq %%rax; "
+        "mulq %2; "
+        "movq $2, %%rbx; "
+        "divq %%rbx, %%rax; "
+        "incq %%rax; "
+        "movq %3, %%rbx; "
+        "addq %%rax, %%rbx; "
+        "movq %%rbx, %3; "
+        "movq %2, %%rax; "
+        "incq %%rax; "
+        "movq %%rax, %2; "
+        "loopq mainloop; "
+        "movq %3, %%rax; "
+        "movq %%rax, %0;"
     
     : "=r" (sum)
     : "r" (n), "r" (index), "r" (sum)
